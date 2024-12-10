af79b751c0af1086f9f20ab62c4348151cb64560
Karina Juarez <a356313@alumnos.uaslp.mx>
2024-12-09 20:57:28 -0600

    sesion 5

---
+++ Scripts/scripts.js
@@ -0,0 +1,6 @@
+console.log("Jajajaja")
+const closeButton = document.getElementById('closeBannerButton')
+const cookieBanner = document.getElementById('cookieBanner')
+closeButton.addEventListener('click', () => {
+  cookieBanner.classList.add('invisible')
+})
